13977e8f83acc52068ed3b74a2ffef9393ae43a3
Adam Knust <acknust@icloud.com>
2025-09-12 18:46:43 -0500

    add google analytics tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,6 +39,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-M7TT0LZP6H"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-M7TT0LZP6H');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${cinzel.variable} ${quattrocento.variable} antialiased`}
       >
